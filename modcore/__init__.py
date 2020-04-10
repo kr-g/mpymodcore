f25be1ae17ef64fe48c6cda707d72e42651c1931
kr-g <k.r.goger+github@gmail.com>
2020-04-10 08:23:15 +0200

    prepared new version

--- modcore/__init__.py
+++ modcore/__init__.py
@@ -14,7 +14,7 @@ from .mod import Module
 from .lifecycle import LifeCycle
 from .log import DEBUG, INFO, NOTSET, logger
 
-VERSION = "v0.0.4"
+VERSION = "v0.0.5a"
 
 print( "-"*41 )
 print( "mpy modcore" )
