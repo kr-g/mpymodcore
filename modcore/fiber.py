e44f977d1a08146aca86e68d3dd8f8160d635df9
kr-g <k.r.goger+github@gmail.com>
2020-04-11 14:35:54 +0200

    added Guard, and Detachable

--- modcore/fiber.py
+++ modcore/fiber.py
@@ -17,10 +17,50 @@ import math
 from modcore.log import LogSupport
 
 
-class FiberLoop(LogSupport):
+class TimerSupport(object):
+    
+    def __init__(self):
+        self.start_timer()
+        
+    def start_timer(self):
+        self.start_time = time.ticks_ms()
+        self.stop_time = 0       
+        self.cpu_time = 0
+        self.lastcall_start = 0
+        self.lastcall_time = 0
+    
+    def run_time(self):
+        return time.ticks_diff(self.stop_time,self.start_time)
+
+    def run_time_diff_ms(self):
+        now = time.ticks_ms()
+        return time.ticks_diff(now,self.start_time)
+
+    def stop_timer(self):
+        self.stop_time = time.ticks_ms()
+        
+    def measure_timer(self,start=True):
+        if start:
+            self.lastcall_start = time.ticks_ms()
+        else:
+            now = time.ticks_ms()
+            self.lastcall_time = time.ticks_diff(now,self.lastcall_start)
+            self.cpu_time += self.lastcall_time
+    
+    def __repr__(self):
+        return self.__class__.__name__ \
+                + "(start: " + str(self.start_time) \
+                +", stop: " + str(self.stop_time) \
+                + ", run time: " + str(self.run_time()) \
+                + ", cpu time: " + str( self.cpu_time ) \
+                + ")"
+
+
+class FiberLoop(LogSupport,TimerSupport):
     
     def __init__(self):
         LogSupport.__init__(self)
+        TimerSupport.__init__(self)
         self.fiber = []
         self._prep()
         
@@ -60,6 +100,7 @@ class FiberLoop(LogSupport):
     
     def __next__(self):
         self._prep()
+        self.measure_timer()
         for f in self.fiber:
             try:
                 r = next(f)
@@ -72,7 +113,9 @@ class FiberLoop(LogSupport):
                 self.fiber.remove(f)
                 self.err.append(f)
                 self.excep(ex, "fiber failed" )
+        self.measure_timer(False)
         if self.all_done():
+            self.stop_timer()
             raise StopIteration
         return self.status()
         
@@ -81,40 +124,40 @@ class FiberLoop(LogSupport):
     
     def __len__(self):
         return len(self.fiber)
-            
-
-class Fiber(LogSupport):
+ 
+        
+class Fiber(LogSupport,TimerSupport):
     
-    def __init__(self,func):
+    def __init__(self,func,ctx=None):
         LogSupport.__init__(self)
+        TimerSupport.__init__(self)
         # save the generator object
         self.func = func
+        self.ctx = None if ctx == None else ctx.spin_off()
         if self.func.__class__.__name__!="generator":
             raise Exception("no generator provided, got", type(self.func) )
-        
-        self.start_time = time.ticks_ms()
-        self.stop_time = 0
+
         self.rc = None
         self.err = None
-        
-        # some more housekeeping
-        self.cpu_time = 0
-        self.lastcall_start = 0
-        self.lastcall_time = 0
-        
+
         # trace performance younter accordigly to the log level
         ## todo
         # change later to debug
         self._perf_counter = self.info()
         
-    def run_time(self):
-        return time.ticks_diff(self.stop_time,self.start_time)
-
-    def run_time_diff_ms(self):
-        now = time.ticks_ms()
-        return time.ticks_diff(now,self.start_time)
-    
+    def close(self):
+        if self.ctx!=None:
+            self.ctx.close()
+            self.ctx=None
+            
+    def spin_off(self):
+        try:
+            return self.ctx.spin_off()
+        finally:
+            self.ctx = None
+                
     def kill(self,reason=None):
+        self.close()
         self.func = None
         self.__kill__(reason)
         
@@ -124,38 +167,35 @@ class Fiber(LogSupport):
     def __next__(self):
         try:
             if self._perf_counter:
-                self.lastcall_start = time.ticks_ms()
+                self.measure_timer()
             
             self.rc = next(self.func)
             
             if self._perf_counter:
-                now = time.ticks_ms()
-                self.lastcall_time = time.ticks_diff(now,self.lastcall_start)
-                self.cpu_time += self.lastcall_time
+                self.measure_timer(False)
             
             return self.rc
         except StopIteration as ex:
             # here we are done
+            self.close()
             raise ex
         except Exception as ex:
             self.err = ex
             self.excep(ex)
+            self.close()
             raise ex
         finally:
-            self.stop_time = time.ticks_ms()
+            self.stop_timer()
       
     def __iter__(self):
         return self
     
     def __repr__(self):
         return self.__class__.__name__ \
-                + "(start: " + str(self.start_time) \
-                +", stop: " + str(self.stop_time) \
-                + ", run time: " + str(self.run_time()) \
-                + ", rc: " + str(self.rc) \
-                + ", cpu time: " + str( self.cpu_time ) \
-                + ("" if self.err==None else repr(err) ) \
-                + ")"
+               + "( rc: " + str(self.rc) \
+                + ("," if self.err==None else repr(self.err) ) \
+                + ") " \
+                + TimerSupport.__repr__(self)
 
 
 class FiberTimeoutException(Exception):
@@ -174,9 +214,62 @@ class FiberWatchdog(Fiber):
         if self.run_time_diff_ms() >= self.max_time_auto_kill_ms:
             raise FiberTimeoutException()
         return super().__next__()
+
+
+class Guard(object):
+    
+    def __init__(self,guard, debug=False ):
+        self._guard = guard
+        self.debug = debug
+
+    def close(self):
+        self.debug and print(">close",self.__class__.__name__)
+        if self._guard != None:
+            self.debug and print(">close-guard",self.__class__.__name__)
+            self._guard.close()
+            self._guard = None
         
-            
-            
+    def __enter__(self):
+        self.debug and print(">enter",self.__class__.__name__)
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.debug and print(">exit",self.__class__.__name__)
+        try:
+            self.__on_exit__(exc_type, exc_value, traceback)
+        finally:
+            self.close()
+    
+    # use this to enhance your code
+    def __on_exit__(self, exc_type, exc_value, traceback):
+        self.debug and print(">onexit",self.__class__.__name__)
+        pass
+    
+    def __call__(self):
+        return self._guard
+    
+    def __repr__(self):
+        return repr(self._guard)
+
+
+class Detachable(Guard):
+    
+    def spin_off(self):
+        self.debug and print( ">detach", self.__class__.__name__ )
+        try:
+            return self.__class__(self._guard,self.debug)
+        finally:
+            self._guard = None            
+
+
+class FiberContext(Detachable):
+       
+    # use this to enhance your code
+    def __on_exit__(self, exc_type, exc_value, traceback):
+        self.debug and print(">leaving",self.__class__.__name__)
+        
+
+        
 
 def sample(path):
     
@@ -194,8 +287,6 @@ def sample(path):
         with open(path) as f:
             while True:
                 
-                # the basic idea                
-                #
                 # do a portion of work, and yield
                 #
                 # important:
@@ -227,9 +318,110 @@ def sample(path):
             
     last_status = fl.status()
     print(last_status)
+
+    print(fl)
+    
+    
+
+def sample2(path,sample_no=0):
+    
+    #fibers only accept generator functions, so a yield is required
+    def _print_final_message(a):
+        print(a)
+        # return code for fbr.rc
+        yield 153
+        
+    def _send_chunk_file(buffer_size,name,f,floop):
+        
+        # do a portion of work, and yield
+        
+        while True:
+            c = f.read(buffer_size)
+            c = c.replace("\r",".")
+            c = c.replace("\n",".")
+            print(name,c)
+            if len(c)==0:
+                break                
+            # yield code control 
+            yield
+        
+        floop.add( Fiber( _print_final_message("\n***done "+ name +"\n" ) ) )
+        
+    def _send_chunk(buffer_size,name,fbrctx,floop):
+        print("fbrctx",fbrctx)
+        #
+        # remember fbrctx is a context manager
+        # if not used continuously in that way __exit__
+        # will be not called, so spin_off here again
+        # it will just copy the guard, not much overhead
+        #
+        with fbrctx.spin_off() as fc:
+            # get the file from the context by calling the context
+            f = fc()
+            while True:
+                
+                # do a portion of work, and yield
+
+                c = f.read(buffer_size)
+                c = c.replace("\r",".")
+                c = c.replace("\n",".")
+                print(name,c)
+                if len(c)==0:
+                    break                
+                # with yield code control 
+                yield
+        
+        floop.add( Fiber( _print_final_message("\n***done "+ name +"\n" ) ) )   
+
+
+    fl = FiberLoop()
+
+    #
+    # use directly as context manager
+    
+    if sample_no==0:   
+        with FiberContext(open(path),debug=True) as fbrctx:
+            print("fbrctx",fbrctx)
+            # if the guard is not move out right here it will be closed
+            fl.add( Fiber( _send_chunk(15,"eins", fbrctx.spin_off(), fl ) ) )
+
+    #
+    # or ... hand it over and do the with block in the fiber function
+    
+    if sample_no==1:   
+        fbrctx = FiberContext(open(path),debug=True)
+        print("fbrctx",fbrctx)
+        fl.add( Fiber( _send_chunk(15,"zwei", fbrctx, fl ) ) )        
+
+    #
+    # or ... let the fiber itself do spin_off, and close 
+    # and have a more simple fiber func notation
+    # this will work good in scenarios where the guard is not handed
+    # further downwards
+    
+    if sample_no==2:   
+        fbrctx = FiberContext(open(path),debug=True)
+        print("fbrctx",fbrctx)
+        f = fbrctx()
+        fl.add( Fiber( _send_chunk_file(15,"drei", f, fl ), ctx=fbrctx ) )
+    
+    # the fiber loop
+
+    for status_change in fl:
+        if status_change:
+            print(status_change)
+            
+    last_status = fl.status()
+    print(last_status)    
+    
+    # print the summary
+    print()
+    print(fl)
+    
     
     # try
-    # from modcore.fiber import sample
+    # from modcore.fiber import sample, sample2
     # sample("boot.py")
+    # sample2("boot.py",sample_no)  # sample_no = 0..2
     
     
