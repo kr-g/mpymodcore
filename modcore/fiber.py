359d2bb0bec59788e851b5ca1c3503cce9724de3
kr-g <k.r.goger+github@gmail.com>
2020-04-16 19:33:13 +0200

    comment

--- modcore/fiber.py
+++ modcore/fiber.py
@@ -7,7 +7,7 @@
     https://en.wikipedia.org/wiki/Cooperative_multitasking
     
     (c)2020 K. Goger (k.r.goger@gmail.com)
-    
+    legal: https://github.com/kr-g/mpymodcore/blob/master/LICENSE
 """
 
 
