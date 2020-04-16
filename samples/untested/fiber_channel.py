113ba25b2f3204c5b7cdb1919e45d57331f8f39d
kr-g <k.r.goger+github@gmail.com>
2020-04-16 18:04:13 +0200

    misc

--- samples/untested/fiber_channel.py
+++ samples/untested/fiber_channel.py
@@ -26,6 +26,15 @@ class DataChunk(object):
         data = self.data[:pos]
         self.data = self.data[pos+1:]
         return data
+    
+    def merge(self):
+        if self.pre!=None:
+            self.data += self.pre.data
+            # keep order
+            self.pre.data = None
+            self.pre = self.pre.pre
+            # done
+        return self
 
     def close(self):
         self.data = None
@@ -95,11 +104,11 @@ class FiberChannel(object):
         self._broken = True
         
     def dense(self):
-        if self.last == None or self.last.pre == None:
-            return False
-        data = self.pop()
-        self.last.data = data + self.last.data
-        return True
+        if self.last == None:
+            return
+        if self.last.pre == self.first:
+            self.first = self.last
+        self.last.merge()
     
     def data_available(self):
         return self.last != None
@@ -135,6 +144,10 @@ def fc_readline(fc,max_size_dense=128):
     
     if chunk.pre==None:
         if fc._broken:
+            # return pending bytes
+            if len(chunk)>0:
+                chunk = fc.pop()
+                return chunk
             raise FiberChannelBrokenException()
         return None
     
@@ -176,5 +189,7 @@ def sample():
                 print(l.decode())
             else:
                 print(">dense")
+        
 
 
+sample()
