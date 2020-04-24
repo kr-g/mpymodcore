35ee9926a023ba10fe15c43e480633f300f7a20e
kr-g <k.r.goger+github@gmail.com>
2020-04-24 07:15:15 +0200

    org imports

--- mod3rd/__init__.py
+++ mod3rd/__init__.py
@@ -1,15 +1,8 @@
-"""
 
-this contains modules not supported by modcore directly
-
-"""
 
 print()
 print("*"*37)
 print( "loading unsupported modules" )
 print("*"*37)
 print()
-
-
-import mod3rd.admin_esp.wlan
 
