bc1537401c81656450631a4187d2bae8eac1d263
kr-g <k.r.goger+github@gmail.com>
2020-06-11 10:01:37 +0200

    create full path during save

--- modext/config/config.py
+++ modext/config/config.py
@@ -1,6 +1,9 @@
 
+import os
 import json
+
 from modext.windup.proc import Namespace
+
 
 ## todo, refactor Namespace
 class Config(Namespace):
@@ -22,7 +25,30 @@ class Config(Namespace):
         self.update( cfg )
         
     def save(self):
-        
+        self.ensure_path()
         with open(self._fnam,"w") as f:
             f.write( json.dumps( self ))
     
+    def ensure_path( self, fnam = None ):
+        
+        if fnam==None:
+            fnam = self._fnam
+            
+        dirs = fnam.split("/")
+        dirs.pop(-1)
+        
+        complete = ""
+        for dir in dirs:
+            if len(dir)==0:
+                continue
+            complete +=  "/" + dir 
+            try:
+                os.mkdir( complete )
+            except Exception as ex:
+                pass
+        
+        # this raises exception if path was not created
+        dir = "/".join(dirs)
+        os.stat( dir )
+
+ 
