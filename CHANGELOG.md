42178dbc8b159ae3c656046d2c55f9b6c132dfc4
kr-g <k.r.goger+github@gmail.com>
2020-06-28 12:42:49 +0200

    prepared new version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 
 # Changelog
 
-## next version v0.0.12
+## version v0.0.12
 
 - variable `$id$` as part of ssid name in `softap.cfg` will be expanded to machine unique id
 - intr Counter rework
--- modcore/__init__.py
+++ modcore/__init__.py
@@ -14,7 +14,7 @@ from .mod import Module
 from .lifecycle import LifeCycle
 from .log import DEBUG, INFO, NOTSET, logger
 
-VERSION = "v0.0.12"
+VERSION = "v0.0.13a"
 
 print( "-"*41 )
 print( "mpy modcore" )
