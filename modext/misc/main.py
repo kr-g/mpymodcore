49d76b44d577b85fe0c246e53a75ed2c1bd30531
kr-g <k.r.goger@gmail.com>
2020-09-07 18:15:35 +0200

    changed date

--- modext/misc/main.py
+++ modext/misc/main.py
@@ -7,7 +7,7 @@ from moddev.control import control_serv
 
 debug_mode = True
 
-def loop( cfg, add_loop ):    
+def loop( cfg, add_loop = None ):    
 
     # turn debug level on for more detailed log info
     #modc.change_log_level( DEBUG if debug_mode else None )
@@ -22,8 +22,9 @@ def loop( cfg, add_loop ):
                 logger.warn("soft break")
                 raise KeyboardInterrupt("break signal")
             
-            # other loop eg webserv
-            add_loop()
+            if add_loop != None:
+                # other loop eg webserv
+                add_loop()
             
         except KeyboardInterrupt:
             logger.info( "\ncntrl+c, auto shutdown=", not debug_mode)
